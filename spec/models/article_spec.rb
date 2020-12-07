18a5cae06223105bc37513af2e7fc5bb93bdca5f
Endy Ranaudo <endy@wearesnook.com>
2020-12-07 12:26:09 +0000

    TDD model validations done

---
+++ spec/models/article_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe Article, type: :model do
+  it "should test that the factory is valid" do
+    expect(FactoryBot.build :article).to be_valid
+  end
+  
+  it "should validate the presence of title" do
+    article = FactoryBot.build :article, title: ''
+    expect(article).not_to be_valid
+    expect(article.errors.messages[:title]).to include("can't be blank")
+  end
+  
+  it "should validate the presence of content" do
+    article = FactoryBot.build :article, content: ''
+    expect(article).not_to be_valid
+    expect(article.errors.messages[:content]).to include("can't be blank")
+  end
+
+  it "should validate the presence of slug" do
+    article = FactoryBot.build :article, slug: ''
+    expect(article).not_to be_valid
+    expect(article.errors.messages[:slug]).to include("can't be blank")
+  end
+
+  it "should validate uniqueness of the slug" do
+    article = FactoryBot.build :article
+    invalid_article = FactoryBot.build :article, article.slug
+    expect(invalid_article).to_not be_valid
+  end
+  
+end
