953147cb79e507002cdb143cdf2b5db4231935f6
Endy Ranaudo <endy@wearesnook.com>
2020-12-07 12:35:51 +0000

    TDD model validations - refactor FactoryBot

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,32 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  it "should test that the factory is valid" do
-    expect(FactoryBot.build :article).to be_valid
-  end
-  
-  it "should validate the presence of title" do
-    article = FactoryBot.build :article, title: ''
-    expect(article).not_to be_valid
-    expect(article.errors.messages[:title]).to include("can't be blank")
-  end
-  
-  it "should validate the presence of content" do
-    article = FactoryBot.build :article, content: ''
-    expect(article).not_to be_valid
-    expect(article.errors.messages[:content]).to include("can't be blank")
-  end
+  describe '#validations' do
+    it 'should test that the factory is valid' do
+      # The "build" methiod refers to FactoryBot (check line 43 of "spec/rails_helper.rb")
+      expect(build :article).to be_valid
+    end
 
-  it "should validate the presence of slug" do
-    article = FactoryBot.build :article, slug: ''
-    expect(article).not_to be_valid
-    expect(article.errors.messages[:slug]).to include("can't be blank")
-  end
+    it 'should validate the presence of the title' do
+      article = build :article, title: ''
+      expect(article).not_to be_valid
+      expect(article.errors.messages[:title]).to include("can't be blank")
+    end
 
-  it "should validate uniqueness of the slug" do
-    article = FactoryBot.build :article
-    invalid_article = FactoryBot.build :article, article.slug
-    expect(invalid_article).to_not be_valid
+    it 'should validate the presence of the content' do
+      article = build :article, content: ''
+      expect(article).not_to be_valid
+      expect(article.errors.messages[:content]).to include("can't be blank")
+    end
+
+    it 'should validate the presence of the slug' do
+      article = build :article, slug: ''
+      expect(article).not_to be_valid
+      expect(article.errors.messages[:slug]).to include("can't be blank")
+    end
+
+    it 'should validate uniqueness of the slug' do
+      article = create :article
+      invalid_article = build :article, slug: article.slug
+      expect(invalid_article).not_to be_valid
+    end
   end
-  
 end
